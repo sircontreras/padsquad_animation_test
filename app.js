a998b547f050063c59eb24c63fd4b477fa66ce78
Rainy Contreras <rainy.contreras@kaptyn.com>
2024-07-30 21:58:51 -0400

    Refactor animation sequences and add missing IDs
    Updated `app.js` to refactor animation sequences into separate functions for better modularity and readability. Also added missing IDs to relevant elements in `index.html` to ensure the animations target the correct elements.

--- app.js
+++ app.js
@@ -1,25 +1,67 @@
-const introCirclesTimeline = gsap.timeline();
-// introCirclesTimeline.to('.c-introBorderItem.fuchsia', {
-//     scale: 7,
-//     duration: 1,
-//     delay: 0.3,
-// })
-//     .to('.introBorderItem.fuchsia', {
-//         borderWidth: '0px',
-//         duration: 1.5,
-//     }, '<')
-//     .to('.c-introBorderItem.orange',{
-//         scale: 7,
-//         duration: 1
-//     }, '-=1.5')
-//     .to('.introBorderItem.orange', {
-//         borderWidth: '0px',
-//         duration: 1.3,
-//     },  '-=1.2').to('.c-introBorderItem.white',{
-//     scale: 7,
-//     duration: 1
-// }, '-=1.5')
-//     .to('.introBorderItem.white', {
-//         borderWidth: '0px',
-//         duration: 1.3,
-//     },  '-=1.2')
+const introCirclesScene = ()=>{
+    const tl = gsap.timeline();
+     tl.to('.c-introBorderItem.fuchsia', {
+        scale: 7,
+        duration: 1,
+        delay: 0.3,
+    })
+        .to('.introBorderItem.fuchsia', {
+            borderWidth: '0px',
+            duration: 1.5,
+        }, '<')
+        .to('.c-introBorderItem.orange',{
+            scale: 7,
+            duration: 1
+        }, '-=1.5')
+        .to('.introBorderItem.orange', {
+            borderWidth: '0px',
+            duration: 1.3,
+        },  '-=1.2').to('.c-introBorderItem.white',{
+        scale: 7,
+        duration: 1
+    }, '-=1.5')
+        .to('.introBorderItem.white', {
+            borderWidth: '0px',
+            duration: 1.3,
+        },  '-=1.2');
+
+    return tl;
+}
+
+const frameOne = ()=>{
+
+    const tl = gsap.timeline();
+    tl.fromTo('#introducingText',{
+
+        display:'none',
+        x : '30px',
+        y:'80px'
+
+    },{
+        display:'block',
+        x : '0',
+        y:'0',
+        duration:0.8,
+    }).from([
+
+        '#discoverText', '#cashbackText', '#debitText'], {
+        opacity: 0,
+        x: '40px',
+        duration: 0.5,
+        stagger: 0.2,
+    },'=-0.2').fromTo('#discoverBankMemberFDICText', {
+        display:'none'
+    },{
+        display:'block'
+    }, '=-1.3')
+
+    return tl;
+
+
+}
+
+let master = gsap.timeline()
+    .add(introCirclesScene())
+    .add(frameOne(), '=-1.5')
+
+
