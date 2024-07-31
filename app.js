ff311ffd71bf0b142770fb62bf3ee46a99e38a18
Rainy Contreras <rainy.contreras@kaptyn.com>
2024-07-30 21:26:15 -0400

    Add intro animation and restructure project
    Introduced a new intro animation for border items using GSAP, and added color variables in a new _colors.scss file. Renamed and relocated CSS and SCSS files to a 'css' directory for better project structure. Enhanced overall styling in app.scss and app.css to support new elements and layout.

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-gsap.to('#redSquare', {
-    x: 100, duration: 2, delay: 1
-});
+const introCirclesTimeline = gsap.timeline();
+// introCirclesTimeline.to('.c-introBorderItem.fuchsia', {
+//     scale: 7,
+//     duration: 1,
+//     delay: 0.3,
+// })
+//     .to('.introBorderItem.fuchsia', {
+//         borderWidth: '0px',
+//         duration: 1.5,
+//     }, '<')
+//     .to('.c-introBorderItem.orange',{
+//         scale: 7,
+//         duration: 1
+//     }, '-=1.5')
+//     .to('.introBorderItem.orange', {
+//         borderWidth: '0px',
+//         duration: 1.3,
+//     },  '-=1.2').to('.c-introBorderItem.white',{
+//     scale: 7,
+//     duration: 1
+// }, '-=1.5')
+//     .to('.introBorderItem.white', {
+//         borderWidth: '0px',
+//         duration: 1.3,
+//     },  '-=1.2')
