f9d637b934ea6f6397198dd05a873a21cdcbfc84
Rainy Contreras <rainy.contreras@kaptyn.com>
2024-07-31 15:50:42 -0400

    Refactor animation selectors into constants
    Extracted various animation selectors into constants for improved readability and maintainability. This change ensures all selector references are centralized, reducing the risk of errors during future updates.

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
+//GENERAL SELECTORS
+const middleCircleOrange = '#middleCircleOrange';
+const card = '#card';
+const introducingText = '#introducingText';
+const discoverOrangeText = '#discoverOrangeText';
+const cashbackText = '#cashbackText';
+const debitOrangeText = '#debitOrangeText';
+const cashbackOnText = '#cashbackOnText';
+const debitBlueText = '#debitBlueText';
+const middleCircleItemWhite = '.middleCircleItem.white';
+
 const introCirclesScene = ()=>{
+
+    //INTRO SHAPES SELECTORS
+    const c_introBorderItemFuchsia = '.c-introBorderItem.fuchsia';
+    const introBorderItemFuchsia = '.introBorderItem.fuchsia';
+    const c_introBorderItemOrange = '.c-introBorderItem.orange';
+    const introBorderItemOrange = '.introBorderItem.orange';
+    const c_introBorderItemWhite = '.c-introBorderItem.white';
+    const introBorderItemWhite = '.introBorderItem.white';
+
     const tl = gsap.timeline();
-     tl.to('.c-introBorderItem.fuchsia', {
+    tl.to(c_introBorderItemFuchsia, {
         scale: 7,
         duration: 1,
         delay: 0.3,
     })
-        .to('.introBorderItem.fuchsia', {
+        .to(introBorderItemFuchsia, {
             borderWidth: '0px',
             duration: 1.5,
         }, '<')
-        .to('.c-introBorderItem.orange',{
+        .to(c_introBorderItemOrange, {
             scale: 7,
             duration: 1
         }, '-=1.5')
-        .to('.introBorderItem.orange', {
+        .to(introBorderItemOrange, {
             borderWidth: '0px',
             duration: 1.3,
-        },  '-=1.2').to('.c-introBorderItem.white',{
+        }, '-=1.2').to(c_introBorderItemWhite, {
         scale: 7,
         duration: 1
     }, '-=1.5')
-        .to('.introBorderItem.white', {
+        .to(introBorderItemWhite, {
             borderWidth: '0px',
             duration: 1.3,
         },  '-=1.2');
@@ -28,10 +48,14 @@ const introCirclesScene = ()=>{
     return tl;
 }
 
+
 const frameOne = ()=>{
 
+    //FRAME 1 SELECTORS
+    const discoverBankMemberFDICText = '#discoverBankMemberFDICText';
+
     const tl = gsap.timeline();
-    tl.fromTo('#introducingText',{
+    tl.fromTo(introducingText, {
         display:'none',
         x : '30px',
         y:'80px'
@@ -41,17 +65,17 @@ const frameOne = ()=>{
         y:'0',
         duration:0.8,
     }).from([
-        '#discoverOrangeText', '#cashbackText', '#debitOrangeText'], {
+        discoverOrangeText, cashbackText, debitOrangeText], {
         opacity: 0,
         x: '40px',
         duration: 0.5,
         stagger: 0.2,
-    },'=-0.2').fromTo('#discoverBankMemberFDICText', {
+    }, '=-0.2').fromTo(discoverBankMemberFDICText, {
         display:'none',
         z: 0,
     },{
         display:'block'
-    }, '=-1.3').fromTo('#card', {
+    }, '=-1.3').fromTo(card, {
         y:'115px',
         rotation: 25,
     },{
@@ -60,7 +84,7 @@ const frameOne = ()=>{
         duration: 1.2,
         ease:"elastic.out(0.8,0.5)"
     }, '=-0.7')
-        .fromTo('#middleCircleOrange', {
+        .fromTo(middleCircleOrange, {
             scale: 0,
             rotation: 25,
         },{
@@ -75,25 +99,30 @@ const frameOne = ()=>{
 
 const frameTwo = ()=>{
 
+    //FRAME 2 SELECTORS
+    const middleCircleItemGrey = '.middleCircleItem.grey';
+    const circlePulseItem = '.circlePulseItem';
+    const realLifeObjectItem = '.realLifeObjectItem';
+
     const tl = gsap.timeline();
 
-    tl.to('#middleCircleOrange', {
+    tl.to(middleCircleOrange, {
         x: '-80px',
         y: '-140px',
         scale: 0.5,
         borderWidth: 0,
         duration: 0.5
-    }).to('#card', {
+    }).to(card, {
         scale: 1.6,
         x:'30px',
         y:'20px'
-    },'<').to('.middleCircleItem.white', {
+    }, '<').to(middleCircleItemWhite, {
         alpha: 0,
         duration: 0.1
-    }, '=-0.5').to(['#introducingText', '#discoverOrangeText', '#cashbackText', '#debitOrangeText'], {
+    }, '=-0.5').to([introducingText, discoverOrangeText, cashbackText, debitOrangeText], {
         alpha: 0,
         duration: 0.1
-    }, '=-0.3').to('#card',{
+    }, '=-0.3').to(card, {
         rotation: -5,
         scale: 1.5,
         x:'25px',
@@ -101,7 +130,7 @@ const frameTwo = ()=>{
         duration: 0.2,
         ease: "power1.in",
         delay: 0.5,
-    }).to('#card',{
+    }).to(card, {
         rotation: 0,
         scale: 1.6,
         duration: 0.3,
@@ -109,11 +138,11 @@ const frameTwo = ()=>{
         y:'25px',
         ease:"power1.out"
 
-    }).fromTo('.circlePulseItem',{
+    }).fromTo(circlePulseItem, {
         scale: 0.5,
         display:'none',
-        width : gsap.getProperty('#middleCircleOrange', 'width','px'),
-        height : gsap.getProperty('#middleCircleOrange', 'height','px'),
+        width: gsap.getProperty(middleCircleOrange, 'width', 'px'),
+        height: gsap.getProperty(middleCircleOrange, 'height', 'px'),
     }, {
         display:'block',
         scale: 0.8,
@@ -122,21 +151,21 @@ const frameTwo = ()=>{
         duration: 0.9,
         stagger: 0.2
     }, '=-0.3')
-        .to('#middleCircleOrange', {
+        .to(middleCircleOrange, {
             x : '15px',
             y: '-30px',
             scale : 1.4,
             borderWidth: 8,
-        }).fromTo('.middleCircleItem.grey',{
+        }).fromTo(middleCircleItemGrey, {
             display:'none'
         },{
             scale: 19,
             display:'block',
 
-        }, '<').to('#card', {
+    }, '<').to(card, {
             rotation: 50,
             y: '170px'
-        }, '<').fromTo('.realLifeObjectItem', {
+    }, '<').fromTo(realLifeObjectItem, {
             alpha: 0,
             scale: 0.5,
         }, {
@@ -146,13 +175,13 @@ const frameTwo = ()=>{
             stagger: 2,
 
         }, '=-0.1')
-        .to('.realLifeObjectItem', {
+        .to(realLifeObjectItem, {
             alpha: 0,
             y: '20px',
             stagger: 2,
             duration: 0.3
 
-        }, '=-2.3').from(['#cashbackOnText', '#debitBlueText'], {
+        }, '=-2.3').from([cashbackOnText, debitBlueText], {
             x: '20px',
             alpha: 0,
             duration: 0.5,
@@ -161,24 +190,32 @@ const frameTwo = ()=>{
 
     return tl;
 
-
-
 }
 
 const frameThree = ()=>{
+
+    //FRAME 3 SELECTORS
+    const discoverBlueText = '#discoverBlueText';
+    const cashBackDebitText = '#cashBackDebitText';
+    const cashBackOrangeText = '#cashBackOrangeText';
+    const isntJustForText = '#isntJustForText';
+    const creditCardsText = '#creditCardsText';
+    const c_learnMoreBtn = '#c-learnMoreBtn';
+    const btnBlurGlow = '#btnBlurGlow';
+
     const tl = gsap.timeline();
 
-    tl.to('#middleCircleOrange',{
+    tl.to(middleCircleOrange, {
         scale: 0.9,
         x: '0px',
         y: '-5px',
-    }).fromTo('.middleCircleItem.white',{
+    }).fromTo(middleCircleItemWhite, {
         alpha: 1,
         scale: 0
     },{
         scale: 1.01,
         duration: 0.5,
-    }, '<').fromTo('#card', {
+    }, '<').fromTo(card, {
         scale: 1,
     }, {
         rotation: 0,
@@ -187,24 +224,24 @@ const frameThree = ()=>{
         ease:"elastic.out(0.4,0.3)",
         duration: 1
 
-    },'=-0.2').to(['#cashbackOnText', '#debitBlueText'],{
+    }, '=-0.2').to([cashbackOnText, debitBlueText], {
         alpha : 0,
         duration: 0.5,
-    },'<=-0.3').from(['#discoverBlueText', '#cashBackDebitText', '#cashBackOrangeText', '#isntJustForText', '#creditCardsText', '#c-learnMoreBtn'], {
+    }, '<=-0.3').from([discoverBlueText, cashBackDebitText, cashBackOrangeText, isntJustForText, creditCardsText, c_learnMoreBtn], {
         alpha:0,
         x:'20px',
         duration: 0.5,
         stagger: 0.1,
-    },'=-0.9').to('#c-learnMoreBtn #btnBlurGlow',{
+    }, '=-0.9').to(btnBlurGlow, {
         x: '160px',
         duration: 0.2
     }).to(
-        '#cashBackOrangeText',{
+        cashBackOrangeText, {
             scale: 1.1,
             duration: 0.2
         }
     ).to(
-        '#cashBackOrangeText',{
+        cashBackOrangeText, {
             scale: 1,
             duration: 0.3
         }
@@ -213,21 +250,9 @@ const frameThree = ()=>{
     return tl;
 }
 
-const seekTime = 0;
-
-let master = gsap.timeline(
-    {
-    // onComplete : ()=>{
-    //     master
-    //         .seek(seekTime)
-    //         .play();
-    // }
-}
-)
+gsap.timeline()
     .add(introCirclesScene())
     .add(frameOne(), '=-1.5')
     .add(frameTwo())
     .add(frameThree());
-
-master.seek(seekTime)
 
