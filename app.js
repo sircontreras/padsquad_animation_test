3df567203c5688dd6e5e724d5542f4e455ca22aa
Rainy Contreras <rainy.contreras@kaptyn.com>
2024-07-30 22:17:42 -0400

    Add animation to card element and border styling
    Enhanced the card element with an elastic animation and adjusted its z-index. Introduced a new border with the 'borderLimit' ID to the app container, styled to be interactive with a higher z-index. This change ensures better user interaction and visual effects.

--- app.js
+++ app.js
@@ -43,17 +43,25 @@ const frameOne = ()=>{
         y:'0',
         duration:0.8,
     }).from([
-
         '#discoverText', '#cashbackText', '#debitText'], {
         opacity: 0,
         x: '40px',
         duration: 0.5,
         stagger: 0.2,
     },'=-0.2').fromTo('#discoverBankMemberFDICText', {
-        display:'none'
+        display:'none',
+        z: 0,
     },{
         display:'block'
-    }, '=-1.3')
+    }, '=-1.3').fromTo('#card', {
+        y:'115px',
+        rotation: 25,
+    },{
+        y:'0',
+        rotation : 0,
+        duration: 1.2,
+        ease:"elastic.out(0.8,0.5)"
+    }, '=-0.7')
 
     return tl;
 
